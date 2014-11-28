6e56d09e5d4185a2a6c3ee61cd5bc8aaf44ec0ee
David Guttman <david@davidguttman.com>
2014-11-28 13:46:55 -0800

    added comments to example

--- example/index.js
+++ example/index.js
@@ -2,4 +2,11 @@ var Korg = require('../')
 
 korg = new Korg()
 
-korg.on('*', console.log)
+korg.on('*', function(event, value) {
+  console.log(event)
+  // event will be something like one of the following
+  // "knob:1", "slider:9", "button:16", "button:prev"
+  console.log(value)
+  // value will be a number between 0 and 1
+  // buttons will either be 0 or 1
+})
